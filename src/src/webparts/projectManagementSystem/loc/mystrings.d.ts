21f6282d7844f3ad3c7337f9907bd1ae8ccf4533
GipsonRahul <kowshikrahul1503@gmail.com>
2023-06-09 13:14:43 +0530

    Initial Push

---
+++ src/src/webparts/projectManagementSystem/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IProjectManagementSystemWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ProjectManagementSystemWebPartStrings' {
+  const strings: IProjectManagementSystemWebPartStrings;
+  export = strings;
+}
